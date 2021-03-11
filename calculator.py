90145583a39b5f3fbd81fbe3b55ee87c27e99b4a
svafaiet <svafaiet@gmail.com>
2021-03-11 21:28:14 +0330

    Add basic operations

--- calculator.py
+++ calculator.py
@@ -1,1 +1,11 @@
-print("Hello user :)")
+def mult(val1, val2):
+    return val1 * val2
+
+def add(val1, val2):
+    return val1 + val2
+
+def sub(val1, val2):
+    return val1 - val2
+
+def divide(val1, val2)
+    return val1 / val2
