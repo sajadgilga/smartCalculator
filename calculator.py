212f68a60b76970c024cf8a311383e410d2155ce
svafaiet <svafaiet@gmail.com>
2021-03-11 21:30:49 +0330

    Add power, Fix bug

--- calculator.py
+++ calculator.py
@@ -7,5 +7,8 @@ def add(val1, val2):
 def sub(val1, val2):
     return val1 - val2
 
-def divide(val1, val2)
+def divide(val1, val2):
     return val1 / val2
+
+def pow(val1, val2):
+    return val1 ** val2
